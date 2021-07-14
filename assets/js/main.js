bad3208ac1f5d1786063109d83fe587a6b1c72c3
Your Name <you@example.com>
2021-07-15 02:23:11 +0700

    update project

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
     mainSlide.init()
+    menuMobile.init()
+    fixedNav.init()
 });
 
 // main slide
@@ -17,5 +19,43 @@ const mainSlide = {
             autoplay: true,
             autoplayTimeout:5000
         })
+    }
+}
+
+// menu mobile 
+const menuMobile = {
+    init: function() {
+        this.menuMobile()
+    },
+    menuMobile: function() {
+        const menuBtn = $(".menu-btn")
+        const menuMobile = $(".menumobile")
+        const menuOverlay = $(".menumobile-overlay")
+
+        menuBtn.click(function() {
+            menuMobile.addClass("active")
+        })
+
+        menuOverlay.click(function() {
+            menuMobile.removeClass("active")
+        })
+    }
+}
+
+// fixed nav 
+
+const fixedNav = {
+    init: function() {
+        this.fixedNav()
+    },
+    fixedNav: function() {
+        $(window).scroll(function fix_element() {
+            $('.header').css(
+              $(window).scrollTop() > 100
+                ? { 'position': 'fixed', 'top': '0', 'right': '0', 'left': '0'}
+                : { 'position': 'relative', 'top': 'auto' }
+            );
+            return fix_element;
+          });
     }
 }
